cda5f142578508c931b4ec479cb00a53d95aa24a
Alexandra Macovei <macovei.a2@gmail.com>
2020-01-31 12:27:38 +0100

    My_Deck.py

--- My_Deck.py
+++ My_Deck.py
@@ -5,3 +5,14 @@ class Card:
 
     def __repr__(self):
         return '{} of {},'.format(self.value, self.suit)
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        suit = ('Hearts', 'Diamonds', 'Clubs', 'Spades')
+        value = ('A', 'K', 'J', 'Q','10', '9', '8', '7', '6', '5', '4', '3', '2')
+        for s in suit:
+            for v in value:
+                self.cards.append(Card(v, s))
+        print(self.cards)
+        
