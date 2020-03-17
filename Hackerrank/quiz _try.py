38cc0bef901f71feaeba752fa000cd76e2dd6865
Alexandra Macovei <macovei.a2@gmail.com>
2020-03-17 11:07:16 +0100

    timer for quiz project improved

--- Hackerrank/quiz _try.py
+++ Hackerrank/quiz _try.py
@@ -6,7 +6,7 @@ def countdown(num):
         mins, secs = divmod(num, 60)
         hour, mins = divmod(mins, 60)
         timer = str(hour).zfill(2) + ':' + str(mins).zfill(2) + ':' + str(secs).zfill(2)
-        print(timer + '\r', end="")
+        print('\r'+timer, end="")
         time.sleep(1)
         num -= 1
 
