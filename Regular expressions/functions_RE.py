f353a2e365bcc99682be57f92c78ee70c3499ad8
Alexandra Macovei <macovei.a2@gmail.com>
2020-03-18 18:21:16 +0100

    validating phone

--- Regular expressions/functions_RE.py
+++ Regular expressions/functions_RE.py
@@ -20,3 +20,19 @@ def extract_all_phone(input):
 print(extract_phone('new number is 451 3117-231'))
 print(extract_phone(' 451 3117-231nkn'))
 print(extract_all_phone('hi snew number is  451 3117-231 and 465 8973-323'))
+
+
+# checking now if  the phone number we extracted is a valid number
+def valid_phone(func):
+    phone_reg = re.compile(r'\d{3} \d{4}-?\d{3}')
+    match = phone_reg.fullmatch(func)
+    if match:
+        return True
+    return False
+
+
+print(valid_phone(('new number is 451 3117-231')))
+print(valid_phone('451 3117-231'))
+
+"""it can also be done with the code from the forst function by just adding ^ at the beginning of the sequence and $ at 
+the end to limit where it starts end ends"""
