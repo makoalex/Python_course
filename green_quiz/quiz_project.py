6a9719f9ee556d6d91d9ff8efcca0db801456b3f
Alexandra Macovei <macovei.a2@gmail.com>
2020-03-18 10:24:42 +0100

    18/03/2020

--- green_quiz/quiz_project.py
+++ green_quiz/quiz_project.py
@@ -4,9 +4,9 @@ with open('quiz.csv', 'a', newline="") as file:
     headers = [' QUESTION ', ' OPTION I ', ' OPTION II ', ' OPTION III ', ' OPTION IV ']
     csv_writer = DictWriter(file, fieldnames=headers)
     csv_writer.writerow({
-        ' QUESTION ': 'Which of the fallowing is a synonym for naive?',
-        ' OPTION I ': 'QUICK- WITTED',
-        ' OPTION II ': 'SLIM- FIGURED',
-        ' OPTION III ': 'CREDULOUS',
-        ' OPTION IV ': 'RIPE'
+        ' QUESTION ': 'Where is a GREEN MOUNTAIN BOY from?',
+        ' OPTION I ': 'VERMONT',
+        ' OPTION II ': 'BRITISH COLUMBIA',
+        ' OPTION III ': 'HAWAII',
+        ' OPTION IV ': 'IRELAND'
     })
