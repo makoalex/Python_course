e2ea6eb0a8dae849b49e5ef1f494d910ec4edd84
Alexandra Macovei <macovei.a2@gmail.com>
2020-03-23 16:07:04 +0100

    project

--- green_quiz/quiz_project.py
+++ green_quiz/quiz_project.py
@@ -1,4 +1,18 @@
 from csv import DictWriter
+from random import choice
+
+
+class Question:
+    def __init__(self, question):
+        self.question = question
+
+    def __repr__(self):
+        return "Green is a versatile word: it can be a noun, adjective, or verb;\n" \
+               " we earn greenbacks to buy greens in the market \n" \
+               "(locally sourced and organic, of course, to be as green as we can be)\nOh yeah, and it's a color too."
+
+    def ask_question(self):
+
 
 with open('quiz.csv', 'a', newline="") as file:
     headers = [' QUESTION ', ' OPTION I ', ' OPTION II ', ' OPTION III ', ' OPTION IV ']
@@ -10,3 +24,4 @@ with open('quiz.csv', 'a', newline="") as file:
         ' OPTION III ': 'HAWAII',
         ' OPTION IV ': 'IRELAND'
     })
+
